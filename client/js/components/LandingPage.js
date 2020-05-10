4ea9722ef0b38eb7afc22ff168dcec34a7a8f84b
Francesco Sullo <sullof@gmail.com>
2020-05-10 00:10:50 -0700

    Dynamic data

--- client/js/components/LandingPage.js
+++ client/js/components/LandingPage.js
@@ -1,103 +1,58 @@
 import Basic from './Basic'
+import data from './data.json'
 
-const {Grid, Row, Col, Button, Badge} = ReactBootstrap
-
+const {Grid, Row, Col} = ReactBootstrap
 
 class LandingPage extends Basic {
 
-  // componentDidMount () {
-  //   document.title = "Tweedentity - A self-claimed identity system"
-  // }
-
-  goToApp() {
-    location.href = `${location.protocol}//dapp.${location.host}/#/connecting`
-  }
-
   render() {
+
+    let total = 12
+    let c = 4
+    let md = 3
+
+    let rows = [
+      <Row>
+        <Col md={total}>
+          <h2 style={{textAlign: 'center'}}>
+            <img src="img/blackglassed.jpg" style={{width: 40, margin: 8, borderRadius: 20}}/><br/>
+            Francesco<br/>
+            Sullo
+          </h2>
+        </Col>
+      </Row>
+    ]
+    let cols = []
+
+    for (let i = 0; i < data.length; i++) {
+
+      let content = []
+      for (let c of data[i].content) {
+        if (!c) {
+          content.push(<br/>)
+        } else if (Array.isArray(c)) {
+          content.push(<a href={c[0]} target="_blank">{c[1]}</a>)
+        } else {
+          content.push(c)
+        }
+      }
+
+      cols[i % c] = <Col md={md}>
+        <h3>{data[i].title}</h3>
+        <p>{content}</p>
+      </Col>
+
+      if (i % c === c - 1 || i === data.length - 1) {
+        rows[Math.floor(i / c) + 1] = <Row>{cols}</Row>
+        cols = []
+      }
+
+    }
 
     return (
       <div>
-      <Grid>
-        <Row>
-
-          <Col md={12}>
-
-            <h2 style={{textAlign: 'center'}}>
-              <img src="img/blackglassed.jpg" style={{width:40, margin:8}}/><br/>
-              Francesco Sullo
-            </h2>
-          </Col>
-        </Row>
-          <Row>
-
-            <Col md={6}>
-              <h3><i className="fab fa-linkedin"></i> job</h3>
-              <p>
-                I quit my job as Technical Lead Manager at Tron Foundation in January to take a break from technology and do some art. More info in my <a href="https://www.linkedin.com/in/francescosullo" target="_blank">LinkedIN</a>.
-              </p>
-            </Col>
-            <Col md={6}>
-              <h3><i className="fab fa-github"></i> open source</h3>
-              <p>I'm active on many projects, some related with blockchain (<a href="https://0xnil.com" target="_blank">0xNIL</a>, <a href="https://tweedentity.com" target="_blank">Tweedentity</a>, 0xAuth), some with secrets management (<a href="https://github.com/secrez/secrez" target="_blank">Secrez</a>). Take a look at my <a href="https://github.com/sullof" target="_blank">GitHub</a>.
-              </p>
-            </Col>
-          </Row>
-          <Row>
-
-            <Col md={6}>
-              <h3><i className="fab fa-amazon"></i> books</h3>
-              <p>If you can read Italian, I hope you will like my short novel "Giulia, io e lo scoglio" on <a
-                href="https://books.apple.com/us/book/giulia-io-e-lo-scoglio/id1441562895"
-                target="_blank">Apple Books</a> for $0.99, or <a
-                href="https://www.amazon.com/Giulia-io-lo-scoglio-Italian-ebook/dp/B07K7ZK5CZ"
-                target="_blank">Amazon</a> (for $1.99, who knows why).
-              </p>
-            </Col>
-            <Col md={6}>
-              <h3><i className="fab fa-soundcloud"></i> music</h3>
-              <p>I was an active musician for years. I am trying to restart. Listen my 2006 EP <a href="https://soundcloud.com/sullof/sets/cinque-canzoni" target="_blank">"Cinque canzoni"</a> on SoundCloud. More will come soon.
-              </p>
-            </Col>
-          </Row>
-          <Row>
-
-            <Col md={6}>
-              <h3><i className="fab fa-twitter"></i> news</h3>
-              <p>
-                I tweet passionately, mostly about cryptos, politics, and freedom. <a href="https://twitter.com/sullof" target="_blank">Follow me</a>.
-              </p>
-            </Col>
-            <Col md={6}>
-              <h3><i className="fab fa-facebook"></i> friends</h3>
-              <p>Even if recently I am not very talkative, I like my friends. You can be my next one on <a href="https://fb.me/francesco.sullo" target="_blank">Facebook</a>.
-              </p>
-            </Col>
-          </Row>
-          <Row>
-
-            <Col md={6}>
-              <h3><i className="fab fa-instagram"></i> visual arts</h3>
-              <p>Since the beginning of the year I am painting. I will make a website for that sooner or later, for now, you can follow me on <a href="https://instagram.com/sullof" target="_blank">Instagram</a>.
-              </p>
-            </Col>
-            <Col md={6}>
-              <h3><i className="fab fa-vimeo"></i> movies</h3>
-              <p>For a while, I made a few short movies, either alone or with friends. You can watch some of them on <a href="https://vimeo.com/sullof" target="_blank">Vimeo</a>.
-              </p>
-            </Col>
-          </Row><Row>
-
-            <Col md={6}>
-              <h3><i className="fab fa-medium"></i> blog</h3>
-              <p>Recently, I wrote on Medium mostly about 0xNIL and other software projects. <a href="https://medium.com/@sullof" target="_blank">Take a look</a>.
-              </p>
-            </Col>
-            <Col md={6}>
-              <h3><i className="fa fa-envelope"></i> contact me</h3>
-              <p>You can find me on Telegram, Signal, WhatsApp and many others messengers. But if you love emails, contact me <a href="mailto:francesco@sullo.co" target="_blank">the old way</a>.
-              </p>
-            </Col>
-          </Row>
+        <Grid>
+          {rows}
         </Grid>
       </div>
     )
@@ -105,3 +60,5 @@ class LandingPage extends Basic {
 }
 
 export default LandingPage
+
+
