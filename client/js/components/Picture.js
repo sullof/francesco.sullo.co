43d87ee3d369627b326d5e662096d06c5af0ef75
Francesco Sullo <sullof@gmail.com>
2020-09-30 20:01:11 -0700

    Adding the blog

--- client/js/components/Picture.js
+++ client/js/components/Picture.js
@@ -3,13 +3,17 @@ class Picture extends React.Component {
   render() {
 
     let data = this.props.data
+    let link = data.link
+    let img = <img
+      width={data.width ? data.width : null}
+      height={data.height ? data.height : null}
+      src={data.src}/>
 
-    return (
-      <img
-        width={data.width ? data.width : null}
-        height={data.height ? data.height : null}
-        src={data.src}/>
-    )
+    if (link) {
+      return <a href={link} target="_blank">{img}</a>
+    } else {
+      return img
+    }
 
   }
 }
