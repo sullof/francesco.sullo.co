2a20bf963de9ae65eee97e1bda4b6102b5793ee4
Francesco Sullo <sullof@gmail.com>
2020-09-30 21:02:31 -0700

    Adding more social accounts

--- client/js/components/Socials.js
+++ client/js/components/Socials.js
@@ -15,14 +15,15 @@ class Socials extends React.Component {
           {getI('https://twitter.com/sullof', 'twitter')}
           {getI('https://facebook.com/francescosullo', 'facebook')}
           {getI('https://instagram.com/sullof', 'instagram')}
-          <br/>
           {getI('https://github.com/sullof', 'github')}
           {getI('https://www.linkedin.com/in/francescosullo/', 'linkedin')}
-          {getI('https://pinterest.com/sullof', 'pinterest')}
           <br/>
+          {getI('https://pinterest.com/sullof', 'pinterest')}
           {getI('https://www.goodreads.com/author/show/20436449.Francesco_Sullo', 'goodreads')}
           {getI('https://vimeo.com/francescosullo', 'vimeo')}
           {getI('https://medium.com/@sullof', 'medium')}
+          {getI('https://www.youtube.com/channel/UCe7mSAskM-qw-xRpVxqc9NQ', 'youtube-square')}
+          <br/>
 
           <div className="linkEmail">
           <a href="mailto:francesco@sullo.co">francesco@sullo.co</a></div>
