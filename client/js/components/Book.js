7770e4ea19e57c6698b178fb2838a07fb5184cb2
Francesco Sullo <sullof@gmail.com>
2020-09-30 20:26:28 -0700

    Add cover by Gael

--- client/js/components/Book.js
+++ client/js/components/Book.js
@@ -10,13 +10,7 @@ class Book extends React.Component {
         width={data.width ? data.width : null}
         height={data.height ? data.height : null}
         src={data.src}/>
-        <div className="links">
-          <div className="mb4">You can buy it on</div>
-        {data.amazon ? <div className="item"><a href={data.amazon} target="_blank"><i className="fab fa-amazon mr6"></i> Amazon</a></div> : null}
-        {data.apple ? <div className="item"><a href={data.apple} target="_blank"><i className="fab fa-apple mr6"></i> Apple Books</a></div> : null}
-        <div  className="mt4">and on the most common online book stores.</div>
         </div>
-      </div>
     )
 
   }
