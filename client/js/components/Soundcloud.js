e387930ffadad82dbfb3d0421bdceaf29eba26a6
Francesco Sullo <sullof@gmail.com>
2020-10-12 17:56:45 -0700

    Adding images

--- client/js/components/Soundcloud.js
+++ client/js/components/Soundcloud.js
@@ -4,30 +4,46 @@ class Soundcloud extends React.Component {
 
     let data = this.props.data
 
+    let img = null
+
+    if (data.src) {
+      img = <img
+        className="soundCover"
+        width={data.width ? data.width : null}
+        height={data.height ? data.height : null}
+        src={data.src}/>
+    }
+
+    let iframe = null
+
     if (data.tracks) {
 
-      return (
-        <iframe
+      iframe = <iframe
           width={data.width ? data.width : '100%'}
           height={data.height ? data.height : 166}
           scrolling="no"
           frameBorder="no"
           allow="autoplay"
           src={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${data.tracks}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=true`}/>
-      )
+
     } else {
 
-      return (
-        <iframe
+      iframe = <iframe
           width={data.width ? data.width : '100%'}
           height={data.height ? data.height : 450}
           scrolling="no"
           frameBorder="no"
           allow="autoplay"
           src={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/${data.playlists}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true`}/>
-      )
 
     }
+
+    return (
+      <div>
+        {img}
+        {iframe}
+      </div>
+    )
   }
 }
 
