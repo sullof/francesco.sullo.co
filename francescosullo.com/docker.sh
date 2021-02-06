87ef075b62ff67d86ec3561a3f1fad8e32c0a912
Francesco Sullo <sullof@gmail.com>
2021-02-06 11:45:02 -0800

    Adding francescosullo.com

--- francescosullo.com/docker.sh
+++ francescosullo.com/docker.sh
@@ -1,7 +1,10 @@
-docker run --name francescosullo-com \
-  -p 8040 \
-  --restart unless-stopped \
-  -e VIRTUAL_HOST=francescosullo.com,www.francescosullo.com \
-  -e LETSENCRYPT_HOST=francescosullo.com,www.francescosullo.com \
-  -e LETSENCRYPT_EMAIL=francescosullo@sameteam.co \
-  -v ./html:/usr/share/nginx/html:ro -d nginx
+echo `pwd`
+
+#
+#docker run --name francescosullo-com \
+#  -p 8040 \
+#  --restart unless-stopped \
+#  -e VIRTUAL_HOST=francescosullo.com,www.francescosullo.com \
+#  -e LETSENCRYPT_HOST=francescosullo.com,www.francescosullo.com \
+#  -e LETSENCRYPT_EMAIL=francescosullo@sameteam.co \
+#  -v /html:/usr/share/nginx/html:ro -d nginx
